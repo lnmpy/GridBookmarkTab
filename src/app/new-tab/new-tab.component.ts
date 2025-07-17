ac1d068bebf273eeb6cae725226c1be3421d1276
elvis <elvismacak@gmail.com>
2025-07-18 02:16:01 +0800

    simply display all bookmarks in new tab

--- src/app/new-tab/new-tab.component.ts
+++ src/app/new-tab/new-tab.component.ts
@@ -1,8 +1,7 @@
-import { booleanAttribute, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Bookmark, BookmarkService } from '../services/bookmark.service';
-import { SettingsService } from '../services/settings.service';
 
 @Component({
   selector: 'app-new-tab',
@@ -12,29 +11,28 @@ import { SettingsService } from '../services/settings.service';
   styleUrls: ['./new-tab.component.scss'],
 })
 export class NewTabComponent implements OnInit {
-  bookmarks: Bookmark[] = [];
-  loading: boolean = true;
-  error: string | null = null;
-  settings: SettingsService;
+  columns: number = 8;
 
-  constructor(
-    private bookmarkService: BookmarkService,
-    private settingsService: SettingsService,
-  ) {
-    this.settings = settingsService;
-  }
+  bookmarks: Bookmark[] = [];
+  breadcrumb: Bookmark[] = [];
+  currentFolder: Bookmark | undefined;
+
+  constructor(private bookmarkService: BookmarkService) {}
 
   ngOnInit() {
-    this.loading = false;
-    this.bookmarks =
-      (this.bookmarkService.getBookmarks()[0].children || [])[0].children || [];
+    this.bookmarks = this.bookmarkService.getBookmarks()[0].children || [];
+    this.currentFolder = this.bookmarks[0];
+    this.breadcrumb = [this.bookmarks[0]];
   }
 
-  /**
-   * 辅助函数，用于在模板中递归渲染书签。
-   * 这个函数会直接在模板中被调用，它处理单个 Bookmark 对象。
-   */
-  trackByBookmarkId(index: number, bookmark: Bookmark): string {
-    return bookmark.id;
+  openFolder(folder: any) {
+    this.breadcrumb.push(folder);
+    this.currentFolder = folder;
+  }
+
+  navigateToCrumb(crumb: any) {
+    const index = this.breadcrumb.indexOf(crumb);
+    this.breadcrumb = this.breadcrumb.slice(0, index + 1);
+    this.currentFolder = crumb;
   }
 }
