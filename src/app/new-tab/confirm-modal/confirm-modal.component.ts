9583d212332622b4d832c3def6480057ac33d601
elvis <elvismacak@gmail.com>
2025-07-28 22:03:23 +0800

    add  more context-demu

---
+++ src/app/new-tab/confirm-modal/confirm-modal.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { ModalService } from '@app/services/modal.service';
+
+@Component({
+  selector: 'app-confirm-modal',
+  imports: [FormsModule],
+  standalone: true,
+  templateUrl: './confirm-modal.component.html',
+  styleUrls: ['./confirm-modal.component.scss'],
+})
+export class ConfirmModalComponent {
+  private modalService: ModalService = inject(ModalService);
+
+  @Input() title: string = 'Confirm';
+  @Input() confirmButtonClass?: string = 'is-warning';
+  @Input() cancelButtonClass?: string = '';
+
+  @Output() confirm = new EventEmitter<void>();
+
+  onConfirm() {
+    this.confirm.emit();
+    this.modalService.close();
+  }
+
+  onCancel() {
+    this.modalService.close();
+  }
+}
--- src/app/new-tab/new-tab.component.ts
+++ src/app/new-tab/new-tab.component.ts
@@ -20,6 +20,7 @@ import {
 } from '../components/context-menu/context-menu.component';
 import { ModalHostComponent } from '../components/modal-host/modal-host.component';
 import { SettingsModalComponent } from './settings-modal/settings-modal.component';
+import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
 
 @Component({
   selector: 'app-new-tab',
@@ -92,8 +93,14 @@ export class NewTabComponent implements OnInit {
           items.push({
             label: 'Delete',
             action: () => {
-              console.log(`delete bookmark ${bookmark.id}`);
-              // TODO
+              this.modalService
+                .open(ConfirmModalComponent, {
+                  title: 'Confirm to delete bookmark',
+                  confirmButtonClass: 'is-danger',
+                })
+                .instance.confirm.subscribe(() => {
+                  alert(`delete bookmark ${bookmark.id} not support`);
+                });
             },
           });
           break;
@@ -101,31 +108,50 @@ export class NewTabComponent implements OnInit {
           items.push({
             label: 'Open all bookmarks',
             action: () => {
-              bookmark.children?.forEach((bookmark) => {
-                if (bookmark.url) {
-                  chrome.tabs.create({ url: bookmark.url });
-                }
-              });
+              this.modalService
+                .open(ConfirmModalComponent, {
+                  title: 'Confirm to Open all bookmarks',
+                })
+                .instance.confirm.subscribe(() => {
+                  bookmark.children?.forEach((bookmark) => {
+                    if (bookmark.url) {
+                      chrome.tabs.create({ url: bookmark.url });
+                    }
+                  });
+                });
             },
           });
           items.push({
             label: 'Open all in new window',
             action: () => {
-              bookmark.children?.forEach((bookmark) => {
-                if (bookmark.url) {
-                  chrome.windows.create({ url: bookmark.url });
-                }
-              });
+              this.modalService
+                .open(ConfirmModalComponent, {
+                  title: 'Confirm to Open all in new window',
+                })
+                .instance.confirm.subscribe(() => {
+                  chrome.windows.create({
+                    url: bookmark.children
+                      ?.map((b) => b.url)
+                      .filter((u): u is string => !!u),
+                  });
+                });
             },
           });
           items.push({
             label: 'Open all in incognito',
             action: () => {
-              bookmark.children?.forEach((bookmark) => {
-                if (bookmark.url) {
-                  chrome.windows.create({ url: bookmark.url, incognito: true });
-                }
-              });
+              this.modalService
+                .open(ConfirmModalComponent, {
+                  title: 'Confirm to Open all in incognito',
+                })
+                .instance.confirm.subscribe(() => {
+                  chrome.windows.create({
+                    url: bookmark.children
+                      ?.map((b) => b.url)
+                      .filter((u): u is string => !!u),
+                    incognito: true,
+                  });
+                });
             },
           });
           items.push({
@@ -139,33 +165,27 @@ export class NewTabComponent implements OnInit {
         default:
           break;
       }
+    } else {
+      items.push({
+        label: 'Bookmark Manager',
+        action: () => {
+          chrome.tabs.create({ url: 'chrome://bookmarks' });
+        },
+      });
+      items.push({
+        label: 'Settings',
+        action: () => {
+          this.modalService
+            .open(SettingsModalComponent)
+            .instance.confirm.subscribe(() => {
+              this.columns = this.settingsService.getSettings().columns;
+              console.log('用户点击了确认');
+            });
+        },
+      });
     }
-    items.push({
-      label: 'Bookmark Manager',
-      action: () => {
-        chrome.tabs.create({ url: 'chrome://bookmarks' });
-      },
-    });
-    items.push({
-      label: 'Settings',
-      action: () => {
-        console.log('click settings menu');
-        const ref = this.modalService.open(SettingsModalComponent);
-        ref.instance.confirm.subscribe(() => {
-          this.columns = this.settingsService.getSettings().columns;
-          console.log('用户点击了确认');
-        });
-      },
-    });
-    // 动态传菜单项
     this.internalOpenContextMenu(event, items);
   }
-
-  // @HostListener('document:click', ['$event'])
-  // onClickOutside(event: MouseEvent) {
-  //   //prevent default event
-  //   // event.preventDefault();
-  // }
 
   openBookmarkFolder(folder: Bookmark) {
     this.breadcrumb.push(folder);
